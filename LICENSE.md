7a8ae422a3d1b7e9ac8e19a537c6a799149fced9
Stephen R. Veit <srveit@gmail.com>
2015-11-25 13:17:09 -0600

    initial fileS

---
+++ LICENSE.md
@@ -0,0 +1,5 @@
+Copyright (c) 2015 Stephen R. Veit
+
+You may use this library under the terms of the [MIT License][].
+
+[MIT License]: http://opensource.org/licenses/MIT
---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = require('./lib/orchestrate');
+{
+  "name": "loopback-connector-orchestrate",
+  "version": "0.1.0",
+  "description": "LoopBack Orchestrate Connector",
+  "keywords": [
+    "StrongLoop",
+    "LoopBack",
+    "Orchestrate",
+    "DataSource",
+    "Connector"
+  ],
+  "main": "index.js",
+  "scripts": {},
+  "dependencies": {
+    "loopback-connector": "^2.3.0",
+    "orchestrate": "~0.6.0"
+  },
+  "devDependencies": {
+    "gulp": "3.8.7",
+    "jasmine": "^2.3.2",
+    "jasmine-core": "^2.3.4",
+    "jasmine-spec-reporter": "^2.4.0",
+    "loopback-datasource-juggler": "^2.42.0"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/srveit/loopback-connector-orchestrate.git"
+  },
+  "license": "MIT"
+}
